f16b0fb5379c81711a7a626ae7573bec654ef846
typhonrt <support@typhonjs.io>
2021-11-21 21:57:33 -0800

    0.0.10 release

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-export { default }                     from './Eventbus.js';
+import Eventbus                        from './Eventbus.js';
 
 export { default as EventbusProxy }    from './EventbusProxy.js';
 export { default as EventbusSecure }   from './EventbusSecure.js';
 
-// /**
-//  * Provides a main eventbus instance.
-//  *
-//  * @type {Eventbus}
-//  */
-// export const eventbus = new Eventbus('mainEventbus');
-//
-// /**
-//  * Provides an eventbus instance potentially for use with a plugin system.
-//  *
-//  * @type {Eventbus}
-//  */
-// export const pluginEventbus = new Eventbus('pluginEventbus');
-//
-// /**
-//  * Provides an eventbus instance potentially for use for testing.
-//  *
-//  * @type {Eventbus}
-//  */
-// export const testEventbus = new Eventbus('testEventbus');
+export { Eventbus as default };
+
+/**
+ * Provides a main eventbus instance.
+ *
+ * @type {Eventbus}
+ */
+export const eventbus = new Eventbus('mainEventbus');
+
+/**
+ * Provides an eventbus instance potentially for use with a plugin system.
+ *
+ * @type {Eventbus}
+ */
+export const pluginEventbus = new Eventbus('pluginEventbus');
+
+/**
+ * Provides an eventbus instance potentially for use for testing.
+ *
+ * @type {Eventbus}
+ */
+export const testEventbus = new Eventbus('testEventbus');

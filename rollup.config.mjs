2dbeadf2c704f17055a6d8b2d1f4f0d0110c7c0e
typhonrt <support@typhonjs.io>
2023-05-03 23:23:43 -0700

    Remove Babel

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,5 @@
 import path             from 'path';
 
-// import { babel }        from '@rollup/plugin-babel';        // Babel is used for private class fields for browser usage.
-
 import { generateDTS }  from '@typhonjs-build-test/esm-d-ts';
 
 await generateDTS({
@@ -20,45 +18,16 @@ const s_DIST_PATH_BROWSER = './dist/browser';
 // Produce sourcemaps or not.
 const s_SOURCEMAP = true;
 
-// Adds Terser to the output plugins for server bundle if true.
-// const s_MINIFY = typeof process.env.ROLLUP_MINIFY === 'string' ? process.env.ROLLUP_MINIFY === 'true' : true;
-
 export default () =>
 {
-   // Defines potential output plugins to use conditionally if the .env file indicates the bundles should be
-   // minified / mangled.
-   // const outputPlugins = [];
-   // if (s_MINIFY)
-   // {
-   //    outputPlugins.push(terser(terserConfig));
-   // }
-
-   // Reverse relative path from the deploy path to local directory; used to replace source maps path, so that it
-   // shows up correctly in Chrome dev tools.
-   // const relativeDistBrowserPath = path.relative(`${s_DIST_PATH_BROWSER}`, '.');
-
    return [{     // This bundle is for the browser distribution.
          input: ['src/index.js'],
          output: [{
             file: `${s_DIST_PATH_BROWSER}${path.sep}Eventbus.js`,
             format: 'es',
-            // plugins: outputPlugins,
             generatedCode: { constBindings: true },
             sourcemap: s_SOURCEMAP,
-            // sourcemapPathTransform: (sourcePath) => sourcePath.replace(relativeDistBrowserPath, `.`)
-         }],
-         plugins: [
-            // babel({
-            //    babelHelpers: 'bundled',
-            //    presets: [
-            //       ['@babel/preset-env', {
-            //          bugfixes: true,
-            //          shippedProposals: true,
-            //          targets: { esmodules: true }
-            //       }]
-            //    ]
-            // })
-         ]
+         }]
       }
    ];
 };
